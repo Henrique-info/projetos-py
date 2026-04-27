07f05fb835b50af57f2d9c5387fbf5a90a515e1e
João Henrique Galdino de Oliveira <20251041110033@ifrn.local>
2026-04-27 13:19:33 -0300

    atualizando

---
+++ desgraça.py
@@ -0,0 +1,9 @@
+class Animal:
+    def __init__(self, nome, sexo, velocidade, peso, estamina, posicaoX, posicaoY):
+        self.nome = nome
+        self.sexo = sexo
+        self.velocidade = velocidade
+        self.peso = peso
+        self.estamina = estamina
+        self.posicaoX = posicaoX
+        self.posicaoY = posicaoY
